e0e091167c32171ea6ef00f5a08f03f936df9358
Romário Gomes <95441690+rmrgomes@users.noreply.github.com>
2022-01-19 15:54:54 -0300

    desilusão

---
+++ script.js
@@ -0,0 +1,19 @@
+/* Regras Codificador: 
+"e" é convertido para "enter" 
+"i" é convertido para "imes"
+"a" é convertido para "ai"
+"o" é convertido para "ober"
+"u" é convertido para "ufat"
+Apenas letras minúsculas
+Não permite acentuação   
+*/
+
+/* Regras Decodificador: 
+"enter" é convertido para "e" 
+"imes" é convertido para "i"
+"ai" é convertido para "a"
+"ober" é convertido para "o"
+"ufat" é convertido para "u"
+Apenas letras minúsculas
+Não permite acentuação     
+*/
